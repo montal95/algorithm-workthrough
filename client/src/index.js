1783dd87408830af2309b8307239cb56732a4e2a
Sam M <sammontalvojr@gmail.com>
2019-08-26 22:51:24 -0500

    initial commit with minor db setup

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import materializeCSS from "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.querySelector("#root")
+);
